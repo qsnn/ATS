8be17a0362e6fe8a2018a0b2825a56469bd32f05
qsnn <ywx2278202141@qq.com>
2025-11-30 19:31:52 +0800

    添加cors配置文件，添加api说明文档

---
+++ src/main/java/com/platform/ats/config/CorsConfig.java
@@ -0,0 +1,26 @@
+// language: java
+package com.platform.ats.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:63342") // 也可以用 "*" 测试阶段放开
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
